863c33733195fd3efac1b5011990864d12c7ce22
GimYoungPhil <youngphil.gim@gmail.com>
2015-03-03 20:59:06 +0900

    BottlesApp

--- js/bottles/bottle_app.js
+++ js/bottles/bottle_app.js
@@ -4,26 +4,15 @@ define(['bar'], function(BarManager) {
 
     BottlesApp.Router = Marionette.AppRouter.extend({
       appRoutes: {
-        'bottles':          'listBottles',
-        'bottles/:id':      'showBottle',
-        'bottles/:id/edit': 'editBottle'
+        'bottles': 'listBottles'
       }
     });
 
     var API = {
       listBottles: function() {
-        require(['bottles/list/list_controller'], function() {
-          BottlesApp.List.Controller.listBottles();
-        });
-      },
-      showBottle: function(id) {
-        require(['bottles/show/show_controller'], function() {
-          BottlesApp.Show.Controller.showBottle(id);
-        });
-      },
-      editBottle: function(id) {
-        require(['bottles/edit/edit_controller'], function() {
-          BottlesApp.Edit.Controller.editBottle(id);
+        require(['bottles/list/list_controller'], function(ListController) {
+          ListController.listBottles();
+          BarManager.execute('set:active:navbar', 'bottles');
         });
       }
     };
@@ -31,16 +20,6 @@ define(['bar'], function(BarManager) {
     BarManager.on('bottles:list', function() {
       this.navigate('bottles');
       API.listBottles();
-    });
-
-    BarManager.on('bottle:show', function(id) {
-      this.navigate('bottles/' + id);
-      API.showBottle(id);
-    });
-
-    BarManager.on('bottle:edit', function(id) {
-      this.navigate('bottles/' + id + '/edit');
-      API.editBottle(id);
     });
 
     BarManager.addInitializer(function() {
---
+++ js/bottles/list/templates/layout.tpl
@@ -0,0 +1,5 @@
+<div id="panel-region"><!-- panelRegion --></div>
+<div class="row">
+  <div class="col-xs-12 col-md-6" id="list-region"><!-- listRegion --></div>
+  <div class="col-xs-12 col-md-6" id="info-region"><!-- infoRegion --></div>
+</div>
